d2deb418edf562399f00e1dcc404bca8c157cc92
Julia Moskvita <ymoskvita@gmail.com>
2023-03-21 19:47:54 +1100

    user model

--- reactionaries/users/models.py
+++ reactionaries/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    is_approved = models.BooleanField(default=False)
+    email = models.EmailField(max_length=254, unique=True)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
 
 # Create your models here.
