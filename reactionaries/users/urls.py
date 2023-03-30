43fc4b3d2e701402afeeefe7887cd6bf6bac7528
tara0d <114784106+tara0d@users.noreply.github.com>
2023-03-30 19:27:05 +1100

    Admin serializer and view
    Created nester serializer and view for admin to pull details in admin approval page.
    Created URL for new view.

--- reactionaries/users/urls.py
+++ reactionaries/users/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.CustomUserList.as_view()),
     path('<int:pk>/', views.CustomerUserDetail.as_view()),
+    path('admin-user/', views.AdminUserView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
