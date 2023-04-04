f5f5da9930bb5e8ce78a24eb136005524da89270
tara0d <114784106+tara0d@users.noreply.github.com>
2023-04-04 23:13:32 +1000

    Debug turned off

--- reactionaries/reactionaries/settings.py
+++ reactionaries/reactionaries/settings.py
@@ -14,7 +14,6 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-f52$)l7_7h#$5@
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') != 'False'
-DEBUG = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
